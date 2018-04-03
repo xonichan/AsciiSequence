5b7f1f7ea9bc7f8f953f23336b7c2474754698e4
xonichan <oni.chan.box@gmail.com>
2018-04-03 17:16:13 +0300

    Добавлен charAt

--- src/AsciiSequence.java
+++ src/AsciiSequence.java
@@ -13,46 +13,18 @@ public class AsciiSequence {
 
         @Override
         public int length() {
-            return byteArray.length;
+            return this.byteArray.length;
         }
 
-        /**
-         * Returns the {@code char} value at the specified index.  An index ranges from zero
-         * to {@code length() - 1}.  The first {@code char} value of the sequence is at
-         * index zero, the next at index one, and so on, as for array
-         * indexing.
-         * <p>
-         * <p>If the {@code char} value specified by the index is a
-         * <a href="{@docRoot}/java/lang/Character.html#unicode">surrogate</a>, the surrogate
-         * value is returned.
-         *
-         * @param index the index of the {@code char} value to be returned
-         * @return the specified {@code char} value
-         * @throws IndexOutOfBoundsException if the {@code index} argument is negative or not less than
-         *                                   {@code length()}
-         */
         @Override
         public char charAt(int index) {
-            return 0;
+            return (char)byteArray[index];
         }
 
-        /**
-         * Returns a {@code CharSequence} that is a subsequence of this sequence.
-         * The subsequence starts with the {@code char} value at the specified index and
-         * ends with the {@code char} value at index {@code end - 1}.  The length
-         * (in {@code char}s) of the
-         * returned sequence is {@code end - start}, so if {@code start == end}
-         * then an empty sequence is returned.
-         *
-         * @param start the start index, inclusive
-         * @param end   the end index, exclusive
-         * @return the specified subsequence
-         * @throws IndexOutOfBoundsException if {@code start} or {@code end} are negative,
-         *                                   if {@code end} is greater than {@code length()},
-         *                                   or if {@code start} is greater than {@code end}
-         */
         @Override
         public CharSequence subSequence(int start, int end) {
+            CharSequence tmpCS;
+            tmpCS = (CharSequence)byteArray[].toString();
             return null;
         }
 
@@ -73,6 +45,8 @@ public class AsciiSequence {
             System.out.println(tmpByteArray.length);
             AsciiCharSequence test_seq = new AsciiCharSequence(tmpByteArray);
             System.out.println(test_seq.length());
+            System.out.println(test_seq.charAt(3));
+
         }
     }
 
